e956bfee060b914c7717c71306a5745b390d622a
tutorial <yadvendra.sharma234@gmail.com>
2025-07-15 12:51:32 +0530

    2nd day

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -5,6 +5,12 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text("home")));
+    double days = 30;
+    String name = "vikram";
+    return Scaffold(
+      appBar: AppBar(title: Text("home"), backgroundColor: Colors.red),
+      body: Center(child: Text('this is $days days of flutter\n $name')),
+      drawer: Drawer(),
+    );
   }
 }
