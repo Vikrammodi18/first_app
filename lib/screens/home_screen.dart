8242e3b9bd78b78bd5f4fd83a432248d42ff6233
tutorial <yadvendra.sharma234@gmail.com>
2025-07-22 17:22:40 +0530

    some changes

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -26,23 +26,42 @@ class _HomeScreenState extends State<HomeScreen> {
     );
     final decodedData = await jsonDecode(catalogJson);
     final productsData = decodedData["products"];
-    CatalogModel.items = List.from(productsData).map<Item>((item)=> Item.fromMap(item)).toList();
+    CatalogModel.items = List.from(
+      productsData,
+    ).map<Item>((item) => Item.fromMap(item)).toList();
     setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-   
     return Scaffold(
       appBar: AppBar(title: Text("Catelog App")),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: (CatalogModel.items != null && CatalogModel.items!.isNotEmpty) ? ListView.builder(
-          itemCount: CatalogModel.items?.length,
-          itemBuilder: (context, index) =>
-             ItemWidget(item: CatalogModel.items![index])
-          
-        ): Center(child: CircularProgressIndicator(),),
+        child: (CatalogModel.items != null && CatalogModel.items!.isNotEmpty)
+            ? GridView.builder(
+                itemCount: CatalogModel.items!.length,
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  mainAxisSpacing: 16,
+                  crossAxisSpacing: 16,
+                  crossAxisCount: 2,
+                ),
+                itemBuilder: (context, index) {
+                  final item = CatalogModel.items?[index];
+                  return Card(
+                    clipBehavior: Clip.antiAlias,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    child: GridTile(
+                      header: Text(item!.name),
+                      child: Image.network(item!.image),
+                      footer: Text(item.price.toString()),
+                    ),
+                  );
+                },
+              )
+            : Center(child: CircularProgressIndicator()),
       ),
       drawer: MyDrawer(),
     );
